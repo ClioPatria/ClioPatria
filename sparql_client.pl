59769f816151688319f36101aec548db8b3dffe6
Jan Wielemaker <J.Wielemaker@uva.nl>
2010-06-17 11:58:50 +0200

    Enhanced documentation and provide option-processing

--- sparql_client.pl
+++ sparql_client.pl
@@ -3,9 +3,10 @@
     Part of SWI-Prolog
 
     Author:        Jan Wielemaker
-    E-mail:        wielemak@science.uva.nl
+    E-mail:        J.Wielemaker@cs.vu.nl
     WWW:           http://www.swi-prolog.org
-    Copyright (C): 2004-2006, University of Amsterdam
+    Copyright (C): 2004-2010, University of Amsterdam
+			      VU University Amsterdam
 
     This program is free software; you can redistribute it and/or
     modify it under the terms of the GNU General Public License
@@ -33,10 +34,32 @@
 	  [ sparql_query/3,		% +Query, -Row, +Options
 	    sparql_set_server/1		% +Options
 	  ]).
-:- use_module(library('http/http_open')).
+:- use_module(library(http/http_open)).
 :- use_module(sparql_xml_result).
 :- use_module(library(lists)).
 :- use_module(library(rdf)).
+:- use_module(library(option)).
+
+/** <module> SPARQL client library
+
+This module provides a SPARQL client.  For example:
+
+    ==
+    ?- sparql_query('select * where { ?x rdfs:label "Amsterdam" }', Row,
+		    [ host('dbpedia.org'), path('/sparql')]).
+
+    Row = row('http://www.ontologyportal.org/WordNet#WN30-108949737') ;
+    false.
+    ==
+
+Or, querying a local server using an =ASK= query:
+
+    ==
+    ?- sparql_query('ask { owl:Class rdfs:label "Class" }', Row,
+		    [ host('localhost'), port(3020), path('/sparql/')]).
+    true.
+    ==
+*/
 
 
 %%	sparql_query(+Query, -Result, +Options) is nondet.
@@ -44,18 +67,30 @@
 %	Execute a SPARQL query on an HTTP   SPARQL endpoint. Query is an
 %	atom that denotes  the  query.  Result   is  unified  to  a term
 %	rdf(S,P,O) for =construct= queries  and   row(...)  for =select=
-%	queries.
+%	queries.  Options are
+%
+%	    * host(+Host)
+%	    * port(+Port)
+%	    * path(+Path)
+%	    * search(+ListOfParams)
+%
+%	Remaining options are passed to   http_open/3.  The defaults for
+%	Host, Port and Path can be   set  using sparql_set_server/1. The
+%	initial default for port is 80 and path is =|/sparql/|=.
 
 sparql_query(Query, Row, Options) :-
-	sparql_param(host(Host), Options),
-	sparql_param(port(Port), Options),
-	sparql_param(path(Path), Options),
+	sparql_param(host(Host), Options,  Options1),
+	sparql_param(port(Port), Options1, Options2),
+	sparql_param(path(Path), Options2, Options3),
+	select_option(search(Extra), Options3, Options4, []),
 	http_open([ protocol(http),
 		    host(Host),
 		    port(Port),
 		    path(Path),
 		    search([ query = Query
+			   | Extra
 			   ])
+		  | Options4
 		  ], In,
 		  [ header(content_type, ContentType)
 		  ]),
@@ -71,8 +106,11 @@ read_reply(MIME, In, Row) :-
 		     close(In)),
 	xml_result(Result, Row).
 read_reply(Type, In, _) :-
+	read_stream_to_codes(In, Codes),
+	string_to_list(Reply, Codes),
 	close(In),
-	throw(error(domain_error(sparql_result_document, Type), _)).
+	throw(error(domain_error(sparql_result_document, Type),
+		    context(_, Reply))).
 
 sparql_result_mime('application/sparql-results+xml').
 sparql_result_mime('application/sparql-result+xml').
@@ -100,11 +138,14 @@ xml_result(select(_VarNames, Rows), Result) :-
 :- dynamic
 	sparql_setting/1.
 
-sparql_param(Param, Options) :-
-	memberchk(Param, Options), !.
-sparql_param(Param, _Options) :-
+sparql_setting(port(80)).
+sparql_setting(path('/sparql/')).
+
+sparql_param(Param, Options0, Options) :-
+	select_option(Param, Options0, Options), !.
+sparql_param(Param, Options, Options) :-
 	sparql_setting(Param), !.
-sparql_param(Param, _Options) :-
+sparql_param(Param, Options, Options) :-
 	functor(Param, Name, _),
 	throw(error(existence_error(option, Name), _)).
 
@@ -113,10 +154,12 @@ sparql_param(Param, _Options) :-
 %	Set sparql server default options.  Provided defaults are:
 %	host, port and repository.  For example:
 %
-%%		set_sparql_default([ host(localhost),
-%%				     port(8080)
-%%				     repository(world)
+%	    ==
+%		set_sparql_default([ host(localhost),
+%				     port(8080)
+%				     repository(world)
 %				   ])
+%	    ==
 
 sparql_set_server([]) :- !.
 sparql_set_server([H|T]) :- !,
