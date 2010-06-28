5912dc455d1779e291f8b84102a34f2d156586eb
Jan Wielemaker <J.Wielemaker@uva.nl>
2010-06-28 19:03:17 +0200

    Make setup independent from cliopatria files

--- setup.pl
+++ setup.pl
@@ -25,11 +25,6 @@
 :- module(cp_setup,
 	  [ setup/0
 	  ]).
-:- load_files([ 'lib/files',
-		library(lists)
-	      ],
-	      [ silent(true)
-	      ]).
 
 %%	setup
 %
@@ -90,10 +85,9 @@ which(File, Path) :-
 	concat_atom([Dir, File], /, Path),
 	access_file(Path, execute).
 
-
-		 /*******************************
-		 *	     RUN IT		*
-		 *******************************/
+%%	goodbye
+%
+%	Say we are done
 
 goodbye :-
 	current_prolog_flag(windows, true), !,
@@ -103,3 +97,64 @@ goodbye :-
 	halt.
 goodbye :-
 	halt.
+
+
+		 /*******************************
+		 *	       UTIL		*
+		 *******************************/
+
+%%	copy_file_with_vars(+File, +DirOrFile, +Bindings) is det.
+%
+%	As =|cp File DirOrFile|=, while substituting =|@var@|=
+%	from Bindings using copy_stream_with_vars/3.
+
+copy_file_with_vars(File, DirOrFile, Bindings) :-
+	destination_file(DirOrFile, File, Dest),
+	open(File, read, In),
+	open(Dest, write, Out),
+	call_cleanup(copy_stream_with_vars(In, Out, Bindings),
+		     (close(In), close(Out))).
+
+destination_file(Dir, File, Dest) :-
+	exists_directory(Dir), !,
+	atomic_list_concat([Dir, File], /, Dest).
+destination_file(Dest, _, Dest).
+
+
+%%	copy_stream_with_vars(+In:stream, +Out:stream,
+%%			      +Bindings:list(Var=Name)) is det.
+%
+%	Copy all data from In to Out, while replacing =|@var@|=
+%	with a binding from Bindings.
+%
+%	@param Bindings	List of Var=Name
+
+copy_stream_with_vars(In, Out, []) :- !,
+	copy_stream_data(In, Out).
+copy_stream_with_vars(In, Out, Bindings) :-
+	get_code(In, C0),
+	copy_with_vars(C0, In, Out, Bindings).
+
+copy_with_vars(-1, _, _, _) :- !.
+copy_with_vars(0'@, In, Out, Bindings) :- !,
+	get_code(In, C0),
+	read_var_name(C0, In, VarNameS, C1),
+	atom_codes(VarName, VarNameS),
+	(   C1 == 0'@,
+	    memberchk(VarName=Value, Bindings)
+	->  write(Out, Value),
+	    get_code(In, C2)
+	;   format(Out, '@~w', [VarName]),
+	    C2 = C1
+	),
+	copy_with_vars(C2, In, Out, Bindings).
+copy_with_vars(C0, In, Out, Bindings) :-
+	put_code(Out, C0),
+	get_code(In, C1),
+	copy_with_vars(C1, In, Out, Bindings).
+
+read_var_name(C0, In, [C0|T], End) :-
+	code_type(C0, alpha), !,
+	get_code(In, C1),
+	read_var_name(C1, In, T, End).
+read_var_name(C0, _In, [], C0).
