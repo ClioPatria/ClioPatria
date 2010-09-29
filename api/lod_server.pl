21c8add4414f1bd0d4e2f320dc0531b69004e655
Jan Wielemaker <J.Wielemaker@cs.vu.nl>
2010-09-29 12:59:19 +0200

    File header and removed library declaration

--- api/lod_server.pl
+++ api/lod_server.pl
@@ -1,6 +1,33 @@
-:- module(lod_server, []).
+/*  Part of ClioPatria SeRQL and SPARQL server
 
-user:file_search_path(library, cliopatria(lib)).
+    Author:        Michiel Hildebrand
+    E-mail:        michielh@few.vu.nl
+    WWW:           http://www.swi-prolog.org
+    Copyright (C): 2010, VU University Amsterdam
+
+    This program is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public License
+    as published by the Free Software Foundation; either version 2
+    of the License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+    As a special exception, if you link this library with other files,
+    compiled with a Free Software compiler, to produce an executable, this
+    library does not by itself cause the resulting executable to be covered
+    by the GNU General Public License. This exception does not however
+    invalidate any other reasons why the executable file might be covered by
+    the GNU General Public License.
+*/
+
+:- module(lod_server, []).
 
 :- use_module(library(http/thread_httpd)).
 :- use_module(library(http/http_dispatch)).
@@ -64,8 +91,8 @@ lod_request(URLComponents, AcceptList) :- % reply 200
 	uri_describe(URI, RDF),
 	lod_reply(AcceptList, URI, RDF).
 lod_request(URLComponents, _AcceptList) :- % reply 404
- 	 parse_url(URL, URLComponents),
-	 throw(http_reply(not_found(URL))).
+	parse_url(URL, URLComponents),
+	throw(http_reply(not_found(URL))).
 
 
 %%	request_url_components(+Request, -URLComponents)
