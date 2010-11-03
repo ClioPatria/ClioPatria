0d3b6ca8a0eb8feb79291c352b2578c254c79bb5
Jan Wielemaker <J.Wielemaker@cs.vu.nl>
2010-11-03 11:19:38 +0100

    DOC: Skinning example

--- examples/conf.d/skin_fix_menu.pl
+++ examples/conf.d/skin_fix_menu.pl
@@ -7,8 +7,17 @@ This module applies an additional style-file  that changes the behaviour
 of the menu and  search-box,  such  that   it  stays  on  the  page when
 scrolling.
 
-Note that the effect is established by   executing  a directive and thus
-cannot be undone by unloading this file.
+Note  that  the  order  of  resources  inserted  by  html_resource/2  is
+undefined. Ordering can be  forced  by   making  one  resource depend on
+another. In this case, the dependent resource  will always be later than
+the dependee. E.g., if fix_menu.css must be loaded after cliopatria.css,
+add a declaration like this:
+
+    ==
+    :- html_resource(css('fix_menu.css'),
+		     [ requires([css('cliopatria.css')])
+		     ]).
+    ==
 */
 
 :- html_resource(cliopatria,
