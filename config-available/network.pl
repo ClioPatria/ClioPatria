9ba4915af4bcf4e6b04fb8c06df38341ad4e152f
Jan Wielemaker <J.Wielemaker@cs.vu.nl>
2010-11-22 09:25:05 +0100

    Add skeleton code for setting the public host

--- config-available/network.pl
+++ config-available/network.pl
@@ -3,10 +3,14 @@
 
 /** <module> Configure the HTTP port
 
-Change the default port on which the HTTP server listens.
+Change  the  default  port  on  which    the  HTTP  server  listens.  If
+host-detection does not work or this server   is behind a proxy, you may
+also need the public_host/public_port settings.
 
 @see localhost.pl
 */
 
 % :- set_setting_default(http:port, 3020).
+% :- set_setting_default(http:public_host, 'www.example.org').
+% :- set_setting_default(http:public_port, 80).
 
