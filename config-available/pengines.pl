ca58afbb16d30b592050493f9e866ebc8034b1ee
Jan Wielemaker <J.Wielemaker@cs.vu.nl>
2014-05-28 14:03:37 +0200

    Update for renamed pengines module.

--- config-available/pengines.pl
+++ config-available/pengines.pl
@@ -9,9 +9,9 @@ Provide access to ClioPatria's RDF store using pengines.
 @see http://www.swi-prolog.org/pldoc/package/pengines.html
 */
 
-% :- set_setting_default(pengine:time_limit, 60).
-:- set_setting_default(pengine:allow_from, ['127.0.0.1']).
-% :- set_setting_default(pengine:deny_from,  []).
+% :- set_setting_default(pengines:time_limit, 60).
+:- set_setting_default(pengines:allow_from, ['127.0.0.1']).
+% :- set_setting_default(pengines:deny_from,  []).
 
 % DO NOT REMOVE THIS :- endif.
 :- endif.
