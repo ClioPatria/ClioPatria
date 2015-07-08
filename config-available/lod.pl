c2945eb64c8b14d6db4ffa57c8b7cc3adc843a3f
Jan Wielemaker <J.Wielemaker@cs.vu.nl>
2015-07-08 20:41:07 +0200

    FIXED: Wrong example in (direct) lod plugin.

--- config-available/lod.pl
+++ config-available/lod.pl
@@ -21,8 +21,15 @@ _object_.
 @see cliopatria(api/lod)
 */
 
+% The bounded_description option is one of `cbd` (Consise Bounded
+% Description) or `scbd` (Symmetric CBD).  The latter includes triples
+% that have the target resource as object.
+
 % Use this if the URIs resolve directly to this server
-%:- http_handler('/rdf/', lod_api, [prefix]).
+%:- http_handler('/rdf/',
+%		 lod_api([ bounded_description(cbd)
+%			 ]),
+%		 [ prefix ]).
 
 % Use this if the URIs are redirected to this server.
 %:- http_handler('/purl/rdf/',
