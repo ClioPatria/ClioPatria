fb000942bb7dc833b20f5396850d3eff9e610944
Wouter Beek <me@wouterbeek.com>
2015-10-24 08:39:53 +0200

    ADDED: Support for SPARQL query POST requests according to        the SPARQL 1.1 Protocol recommendation.

--- api/sparql.pl
+++ api/sparql.pl
@@ -59,11 +59,74 @@
 %	HTTP  handler  for  SPARQL  requests.    Mounted  the  http-path
 %	sparql(.)       (by       default        =|/sparql/|=,       see
 %	library(http/http_path)).
+%
+%	As part of a SPARQL request the user may specify the following things:
+%	1. `query`
+%	   The contents of the SPARQL query.
+%	   Exactly one must occur in every SPARQL query request.
+%	2. `default-graph`
+%	   The default graph as specified by the SPARQL dataset structure,
+%	   against which the query is evaluated.
+%	   Zero or more default graphs may be specified.
+%	3. `named-graph'
+%	   The named graphs as specified by the SPARQL dataset structure,
+%	   against which the query is evaluated.
+%	   Zero or more named graphs may be specified.
+%
+%	There are three ways of posing a SPARQL query:
+%	1. An HTTP GET request where `query`, `default-graph` and `named-graph`
+%	   appear in the IRI's search string and are all subject to
+%	   IRI-encoding.  Example:
+%	   `curl http://localhost:3020/sparql/?query=select%20*%20where%20%7B%20%3Fs%20%3Fp%20%3Fo%20%7D`
+%	   No Content-Type needs to be specified.
+%	2. An HTTP POST request where `query`, `default-graph`
+%	   and `named-graph` appear in the POST body using IRI search string
+%	   syntax and subject to IRI-encoding.  Example:
+%	   `curl --data "query=select * where { ?s ?p ?o }" http://localhost:3020/sparql/`
+%	   The Content-Type must be `application/x-www-form-urlencoded`.
+%	3. An HTTP POST request where `default-graph` and `named-graph` appear
+%	   in the IRI's search string and are subject to IRI-encoding
+%	   and where the query appears as-is in the POST body.  Example:
+%	   `curl -X POST -H "Content-Type: application/sparql-query" -d @query.sparql http://localhost:3020/sparql/`
+%	   The Content-Type must be `application/sparql-query`.
 
 sparql_query(Request) :-
 	empty_get_request(Request), !,
 	redirect_human_form(Request).
+% Perform a SPARQL query via GET.
+% @compat SPARQL 1.1 Protocol recommendation, section 2.1.1.
 sparql_query(Request) :-
+	memberchk(method(get), Request), !,
+	sparql_query_parameters(Request).
+% Perform a SPARQL query via POST with encoded parameters in body.
+% @compat SPARQL 1.1 Protocol recommendation, section 2.1.2.
+sparql_query(Request) :-
+	memberchk(method(post), Request),
+	memberchk(content_type(ContentType), Request),
+	sub_atom(ContentType, 0, _, _, 'application/x-www-form-urlencoded'), !,
+	catch(sparql_query_parameters(Request), E, sparql_query_exception(E)).
+% Perform a SPARQL query via POST with unencoded body.
+% @compat SPARQL 1.1 Protocol recommendation, section 2.1.3.
+sparql_query(Request) :-
+	memberchk(method(post), Request),
+	memberchk(content_type(ContentType), Request),
+	sub_atom(ContentType, 0, _, _, 'application/sparql-query'), !,
+	http_parameters(Request,
+			[ 'default-graph-uri'(DefaultGraphs),
+			  'named-graph-uri'(NamedGraphs),
+			  format(ReqFormat),
+			  entailment(Entailment)
+			],
+			[ attribute_declarations(sparql_decl)
+			]),
+	append(DefaultGraphs, NamedGraphs, Graphs),
+	http_read_data(Request, Query, []),
+	authorized(read(Graphs, sparql)),
+	sparql_reply(Request, Query, Graphs, ReqFormat, Entailment).
+sparql_query(_) :-
+	throw(http_reply(bad_request(format('Unrecognized SPARQL query request.', [])))).
+
+sparql_query_parameters(Request) :-
 	http_parameters(Request,
 			[ query(Query),
 			  'default-graph-uri'(DefaultGraphs),
@@ -76,6 +139,12 @@ sparql_query(Request) :-
 	append(DefaultGraphs, NamedGraphs, Graphs),
 	authorized(read(Graphs, sparql)),
 	sparql_reply(Request, Query, Graphs, ReqFormat, Entailment).
+
+sparql_query_exception(E) :-
+	E = error(syntax_error(illegal_uri_query),_), !,
+	throw(http_reply(bad_request(format('Malformed search parameters.', [])))).
+sparql_query_exception(E) :-
+	throw(E).
 
 %%	empty_get_request(+Request) is semidet.
 %
